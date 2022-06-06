7d51a6328b65cd6b67f609fecb29c77244dd11bb
Seth Boyle <smb4295@gmail.com>
2022-06-06 19:02:44 -0400

    knowledge check complete

--- Knowledge_Check.py
+++ Knowledge_Check.py
@@ -1,1 +1,10 @@
 print ("hello world!")
+
+golfclubs = ['TaylorMade', 'Callaway', 'Ping', 'Titleist']
+print (golfclubs [1])
+
+golfslang = {'slice':'right','fade':'left'}
+print (golfslang['slice'])
+
+golfscores = (3, 5, 4, 6)
+print (golfscores [3])
